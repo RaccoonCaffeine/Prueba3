4853640a7d983561f7f1a984be22e25e91c17f4b
RaccoonCaffeine <gtpcasa@gmail.com>
2024-07-01 13:05:57 -0400

    Prueba 3 - Base HTML

--- Main/views.py
+++ Main/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 from .models import Navbar
 
 def index(request):
+    titulo = "Home"
     navbars = Navbar.objects.all()
-    context = {'navbar': navbars}
+    context = {'navbar': navbars, 'Titulo': titulo}
     return render(request, 'home.html', context)
 
 def quienes(request):
+    titulo = 'Quienes Somos'
     navbars = Navbar.objects.all()
-    context = {'navbar': navbars}
+    context = {'navbar': navbars, 'Titulo': titulo}
     return render(request,'quienessomos.html',context)
 
 def servicios(request):
+    titulo = 'Servicios'
     navbars = Navbar.objects.all()
-    context = {'navbar': navbars}
+    context = {'navbar': navbars, 'Titulo': titulo}
     return render(request,'servicios.html',context)
